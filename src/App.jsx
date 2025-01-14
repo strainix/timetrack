7631795cc76ac382ece3cbce76f01931c6ec285a
strainix <63642187+strainix@users.noreply.github.com>
2025-01-14 13:50:11 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TimeTracker from './components/TimeTracker'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TimeTracker />
+    </div>
+  )
+}
+
+export default App
